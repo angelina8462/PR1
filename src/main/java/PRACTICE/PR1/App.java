b85f425c729678b672cd1d6ca913eb97b42b074e
Angelina Nemolyaeva <a.nemolyaeva@mail.ru>
2021-07-08 11:41:14 +0300

    Проверка

---
+++ src/main/java/PRACTICE/PR1/App.java
@@ -0,0 +1,13 @@
+package PRACTICE.PR1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! GOOD DAY!" );
+    }
+}
